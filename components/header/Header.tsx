91af6976a936a213d8081878f5a1b83d977e9e45
Basit Olaiya <bolaiya@wazobia.tech>
2022-08-08 01:43:15 +0100

    init portfolio

---
+++ components/header/Header.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const Header = () => {
+  return (
+    <div className="font-monte w-full bg-red-500">
+      <div>
+        <h1>Header tag</h1>
+        <div className="">Normal div</div>
+      </div>
+    </div>
+  );
+};
