c3d28eb2ad10abd30856f02ddbfebd1bb49828c1
Olaiya Basit <bolaiya@wazobia.tech>
2023-01-04 08:41:06 +0100

    brands init

--- components/V2/testimonial/Testimonial.tsx
+++ components/V2/testimonial/Testimonial.tsx
@@ -1,9 +1,20 @@
 import React from "react";
+import { brandData } from "./data";
 
 export const Testimonial = () => {
+  const displayBrands = () =>
+    brandData.map((brand, index) => <img src={brand} key={index} />);
+
   return (
-    <div className="h-[60vh] bg-[#1E1E1E]">
-      <p className="text-white text-[24px] font-bold text-center">Blessed to work with top brands</p>
+    <div className="h-[60vh] bg-[#1E1E1E] ">
+      <div className="max-w-[1280px] m-auto">
+        <p className="text-white text-[24px] font-bold text-center">
+          Blessed to work with top brands
+        </p>
+        <div className="flex justify-center gap-[64px] mt-[40px]">
+          {displayBrands()}
+        </div>
+      </div>
     </div>
   );
 };
---
+++ components/V2/testimonial/data.ts
@@ -0,0 +1,8 @@
+export const brandData = [
+  "/images/jungle.svg",
+  "/images/vt.svg",
+  "/images/brand3.svg",
+  "/images/brand4.svg",
+  "/images/brand5.svg",
+  "/images/brand6.svg",
+];
